4a02e109e83a4a93f5b8154d8a869308116ffeb4
Mario Alvarez <44630882+MrDavidAlv@users.noreply.github.com>
2026-01-04 00:28:58 -0500

    Add files via upload

---
+++ CursoBasicoDeProgramacionEnGO-Platzi/src/interfaces.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+type figura2D interface {
+	area() float64
+}
+
+type cuadrado struct {
+	lado float64
+}
+
+type rectangulo struct {
+	base   float64
+	altura float64
+}
+
+func (c cuadrado) area() float64 {
+	return c.lado * c.lado
+}
+
+func (r rectangulo) area() float64 {
+	return r.base * r.altura
+}
+
+func calcularArea(f figura2D) {
+	fmt.Println("Area:", f.area())
+	// reKturn f.area()
+}
+
+func interfacesEnGo() {
+	myCuadrado := cuadrado{lado: 4}
+	myRectangulo := rectangulo{base: 3, altura: 6}
+
+	// SIN USAR INTERFACES
+	fmt.Println("Área del cuadrado:", myCuadrado.area())
+	fmt.Println("Área del rectángulo:", myRectangulo.area())
+
+	// USANDO INTERFACES
+	calcularArea(myCuadrado)
+	calcularArea(myRectangulo)
+
+	// Usando una lista de interfaces
+	myInterfaces := []interface{}{"Hola", 12, 4.90}
+	fmt.Println(myInterfaces...)
+}
--- CursoBasicoDeProgramacionEnGO-Platzi/src/main.go
+++ CursoBasicoDeProgramacionEnGO-Platzi/src/main.go
@@ -14,4 +14,5 @@ func main() {
 	modificadoresAcceso()
 	punterosGo()
 	stringersGo()
+	interfacesEnGo()
 }
